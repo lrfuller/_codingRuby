a137f3f81b6767f7c71ff7c4176546b7a3d5b03e
lrfuller <33261077+lrfuller@users.noreply.github.com>
2021-03-17 12:14:50 -0400

    step 6 finished

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,4 +2,51 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # maps to http://localhost:3000/articles/new
+  def new
+    @article = Article.new
+  end
+
+  # post call after article formBuilder is completed, checks if the form is valid
+  # then saves
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
